176b2d6ff27a296cb8fa57ddddf6577598105f69
JarMaou <fajarxyz1@gmail.com>
2024-09-22 08:19:46 +0800

    index.html

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log(`Hello World!);
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let comments = []; // Array untuk menyimpan komentar
+
+// Middleware untuk melayani file statis
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Kirim semua komentar yang ada saat pengguna baru terhubung
+    socket.emit('load comments', comments);
+
+    // Dengarkan event 'new comment'
+    socket.on('new comment', (comment) => {
+        comments.push(comment);
+        io.emit('new comment', comment); // broadcast komentar baru ke semua pengguna
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
